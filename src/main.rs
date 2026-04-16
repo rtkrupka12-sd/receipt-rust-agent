827496336353bc89c87d7ca667017b163f8c8c80
Robert Krupka <rtkrupka12@gmail.com>
2026-04-15 22:27:56 -0400

    boilerplate main function to be able to test configuration, along with gitignore and .env.example

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+﻿mod error;
+mod config;
+
+use config::AzureConfig;
+
+fn main() {
+    match AzureConfig::from_env() {
+        Ok(cfg) => {
+            println!("✅ Config Loaded!");
+            println!("Endpoint: {}", cfg.doc_intel_endpoint);
+        }
+        Err(e) => {
+            eprintln!("❌ Configuration Failed: {}", e);
+            std::process::exit(1);
+        }
+    }
+}
